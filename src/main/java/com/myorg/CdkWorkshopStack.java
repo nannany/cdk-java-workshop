3b3c8cd28fe5f1b8173c814f425761f3229331f2
nannany <ymym1990ymym@gmail.com>
2021-06-19 11:03:02 +0900

    apigatewayのとこまで

--- src/main/java/com/myorg/CdkWorkshopStack.java
+++ src/main/java/com/myorg/CdkWorkshopStack.java
@@ -1,12 +1,12 @@
 package com.myorg;
 
 import software.amazon.awscdk.core.Construct;
-import software.amazon.awscdk.core.Duration;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
-import software.amazon.awscdk.services.sns.Topic;
-import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
-import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.apigateway.LambdaRestApi;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
 
 public class CdkWorkshopStack extends Stack {
     public CdkWorkshopStack(final Construct parent, final String id) {
@@ -16,14 +16,15 @@ public class CdkWorkshopStack extends Stack {
     public CdkWorkshopStack(final Construct parent, final String id, final StackProps props) {
         super(parent, id, props);
 
-        final Queue queue = Queue.Builder.create(this, "CdkWorkshopQueue")
-                .visibilityTimeout(Duration.seconds(300))
+        final Function hello = Function.Builder.create(this, "HelloHandler")
+                .runtime(Runtime.NODEJS_10_X)
+                .code(Code.fromAsset("lambda"))
+                .handler("hello.handler")
                 .build();
 
-        final Topic topic = Topic.Builder.create(this, "CdkWorkshopTopic")
-            .displayName("My First Topic Yeah")
-            .build();
+        LambdaRestApi.Builder.create(this, "Endpoint")
+                .handler(hello)
+                .build();
 
-        topic.addSubscription(new SqsSubscription(queue));
     }
 }
